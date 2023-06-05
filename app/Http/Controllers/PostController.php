78af85b95720d406aafdb03cc0b3d19b77d8b9f6
JunWedi <junwedisinghe@gmail.com>
2023-06-05 23:14:45 +0900

    ブログの投稿

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,19 +13,33 @@ use App\Models\Post;
  * @param Post Postモデル
  * @return array Postモデルリスト
  */
+ use App\Http\Requests\PostRequest; // useする
 
 class PostController extends Controller
 {
     
  public function index(Post $post)
- {
+  {
     return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
- } 
+  } 
  
  public function show(Post $post)
- {
+  {
     return view('posts.show')->with(['post' => $post]);
- }
+  }
+ 
+ public function create()
+  {
+    return view('posts.create');
+  }
+  
+ 
+ public function store(Post $post, PostRequest $request) // 引数をRequestからPostRequestにする
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
  
 }
 ?>
