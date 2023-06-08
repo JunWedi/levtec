14a878893f4e9d59d1e16b619199c1de9a55ecd5
JunWedi <junwedisinghe@gmail.com>
2023-06-09 00:04:01 +0900

    ブログ編集

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
+//外部のPostControllerクラスをインポート
+use App\Http\Controllers\PostController;  
 
 /*
 |--------------------------------------------------------------------------
@@ -20,10 +21,25 @@ Route::get('/', function () {
 });
 
 
+//投稿一覧表示
 Route::get('/', [PostController::class, 'index']);
 
+
+//投稿作成画面
 Route::get('/posts/create', [PostController::class, 'create']);
 
+
+//投稿表示
 Route::get('/posts/{post}', [PostController::class ,'show']);
 
+
+//投稿作成
 Route::post('/posts', [PostController::class, 'store']);
+
+
+//投稿編集画面表示
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+
+
+//投稿編集保存
+Route::put('/posts/{post}', [PostController::class, 'update']);
